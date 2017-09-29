854660a5755a67b6b53dca75ff8a2402ba7934ee
SwayGatsby <kayla.fowler@hotmail.com>
2017-09-29 18:01:46 +0100

    four tests passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -12,14 +12,3 @@ def  add_or_remove_cash(pet_shop, cash)
   current_cash = pet_shop[:admin][:total_cash] += cash
     return current_cash
 end
-
-# def test_add_or_remove_cash__add
-#   add_or_remove_cash(@pet_shop,10)
-#   cash = total_cash(@pet_shop)
-#   assert_equal(1010, cash)
-# end
-
-# def add_or_remove_cash(pet_shop, cash)
-#   total_cash = pet_shop[:admin][:total_cash] += cash
-#   return total_cash
-# end
